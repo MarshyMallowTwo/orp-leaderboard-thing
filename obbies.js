9bcd3a6c87ea81db2f2453140d3fd110fd677f75
MarshyMallowTwo <286715986+MarshyMallowTwo@users.noreply.github.com>
2026-06-28 19:00:28 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ obbies.js
@@ -0,0 +1,34 @@
+const obbies = [
+    {
+        name: "High Quality Obby",
+        difficulty: {
+            name: "Intense",
+            value: 6.99
+        },
+        creator: "John Doe"
+    },
+    {
+        name: "Obby of Doom",
+        difficulty: {
+            name: "Terrifying",
+            value: 10.78
+        },
+        creator: "Jane Smith"
+    },
+     {
+        name: "Obby of Fun Things :D",
+        difficulty: {
+            name: "Nil",
+            value: 14.03
+        },
+        creator: "MarshyMallow"
+    },
+    {
+        name: "a verrrrrrrrrrrrrrrrrrrrrrrry long walk",
+        difficulty: {
+            name: "Unreal",
+            value: 13.10
+        },
+        creator: "Not MarshyMallow trust :D"
+    }
+]
---
+++ script.js
@@ -0,0 +1,58 @@
+window.addEventListener("DOMContentLoaded", () => {
+const sidebar = document.getElementById("sidebar");
+const infoBox = document.getElementById("infoBox");
+
+obbies.sort((a, b) => {
+    return b.difficulty.value - a.difficulty.value;
+});
+
+obbies.forEach((obby, index) => {
+    const item = document.createElement("div");
+    item.className = "item";
+
+    const rank = index + 1;
+    const color = difficulties[obby.difficulty.name] || "#FFFFFF";
+
+    item.innerHTML = `
+    <span class="rank" style="color:${color}">#${rank}</span>
+    <span class="name">${obby.name}</span>
+    <span class="value" style="color:${color}">${obby.difficulty.value.toFixed(2)}</span>
+`;
+
+    item.style.borderleft = `6px solid ${color}`;
+
+    item.addEventListener("click", () => {
+    infoBox.innerHTML = `
+    <div class="info-title">
+    ${obby.name}
+    </div>
+
+    <div class="info-stats">
+
+    <div class="stat">
+    <span class="label">Rank</span>
+    <span class="value">#${rank}</span>
+    </div>
+
+    <div class="stat">
+    <span class="label">Difficulty</span>
+
+    <span class="value">
+    <span style="color:${color}">
+    ${obby.difficulty.name} 
+    </span>
+    <span>
+    (${obby.difficulty.value.toFixed(2)})
+    </span>
+    </div>
+
+    <div class="stat">
+    <span class="label">Creator</span>
+    <span class="value">${obby.creator}</span>
+    </div>
+    `;
+});
+
+    sidebar.appendChild(item);
+});
+});
